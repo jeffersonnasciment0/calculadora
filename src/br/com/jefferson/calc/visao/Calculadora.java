1cbc87c7e05e1ebbd9a8ee0c32ee04ed38d05fff
Jefferson Santos <jeffersonnascimento@mobitbrasil.com.br>
2022-07-14 07:18:13 -0300

    Inicializando segundo projeto swing

---
+++ src/br/com/jefferson/calc/visao/Calculadora.java
@@ -0,0 +1,20 @@
+package br.com.jefferson.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame{
+
+	public Calculadora() {
+
+		setSize(332,422);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new Calculadora();
+	}
+}
